939edf1d549424935065e298ca0d8d11b6e0b20f
Lisa Carlson <lisa.carlson85@gmail.com>
2015-09-29 14:44:40 -0600

    Render books from NYT API

--- booksApp/routes/index.js
+++ booksApp/routes/index.js
@@ -4,16 +4,16 @@ var unirest = require('unirest');
 
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
+router.get('/', function(req, res, next) {
+  res.redirect('/books');
+});
 
 router.get('/books', function(req, res) {
     unirest.get('http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=' + process.env.NYT_API_KEY)
       .end(function (response) {
-        console.log(response.body.results.books);
+        var NYTBooks = response.body.results.books;
+        res.render('index', {books: NYTBooks});
       })
-    res.end();
 });
 
 module.exports = router;
