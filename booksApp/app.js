e77f058dd43b48c0a86b1288d540d57e8c947610
Lisa Carlson <lisa.carlson85@gmail.com>
2015-09-29 14:00:40 -0600

    include env variable and logged API response

--- booksApp/app.js
+++ booksApp/app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var unirest = require('unirest');
+require('dotenv').load();
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -31,6 +33,13 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
+// app.get('/books', function(req, res) {
+//     unirest.get('http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=' + NYT_API_KEY)
+//       .end(function (response) {
+//         console.log(response.body);
+//       })
+// });
 
 // error handlers
 
--- booksApp/routes/index.js
+++ booksApp/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var unirest = require('unirest');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router.get('/books', function(req, res) {
+    unirest.get('http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=' + process.env.NYT_API_KEY)
+      .end(function (response) {
+        console.log(response.body.results.books);
+      })
+    res.end();
 });
 
 module.exports = router;
